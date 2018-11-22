2325f1040d6595bf177caed5ed5cfcd34e5d81a7
tsttang2018 <44975425+tsttang2018@users.noreply.github.com>
2018-11-23 00:37:10 +0800

    Update upload_score.php

--- upload_score.php
+++ upload_score.php
@@ -1,14 +1,15 @@
 <?php
 // Create connection
-$con = new mysqli("server_name", "username","password";
-
+$con = new mysqli("server_name", "root","","snaker";
+if($con)
+         echo "Your score is uploaded";
 // Check connection
 if ($con->connect_error){
 echo "Failed to connect to MySQL: ".$con->connect_error;
 }
-
+$name  = $_POST["name"];
 $score = $_POST["score"];
-$status = $con->query("INSERT INTO player_score (score) VALUES ($score)");
+$status = $con->query("INSERT INTO player_score (name,score) VALUES ('$name',$score)");
 if(!$status){
 echo ("Error in inserting the score");
 }
