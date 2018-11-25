88ccf308b8ce1d1b358ba974e2f44ce455f99318
tsttang <szeting430@gmail.com>
2018-11-25 20:53:46 +0800

    show ranking

--- js/ranking.js
+++ js/ranking.js
@@ -9,6 +9,24 @@ var rankingJS = {
              console.log(data);
              $("#name-1").html(data[0]['username']);
              $("#score-1").html(data[0]['score']);
+             $("#name-2").html(data[1]['username']);
+             $("#score-2").html(data[1]['score']);
+             $("#name-3").html(data[2]['username']);
+             $("#score-3").html(data[2]['score']);
+             $("#name-4").html(data[3]['username']);
+             $("#score-4").html(data[3]['score']);
+             $("#name-5").html(data[4]['username']);
+             $("#score-5").html(data[4]['score']);
+             $("#name-6").html(data[5]['username']);
+             $("#score-6").html(data[5]['score']);
+             $("#name-7").html(data[6]['username']);
+             $("#score-7").html(data[6]['score']);
+             $("#name-8").html(data[7]['username']);
+             $("#score-8").html(data[7]['score']);
+             $("#name-9").html(data[8]['username']);
+             $("#score-9").html(data[8]['score']);
+             $("#name-10").html(data[9]['username']);
+             $("#score-10").html(data[9]['score']);
              }});
       
     $("#ranking-back").on("click", function(){
